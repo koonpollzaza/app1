46a4e659f3f47d18f2f1309ac68756c7c2e61560
koonpollzaza <152575123+koonpollzaza@users.noreply.github.com>
2025-02-19 15:13:32 +0700

    Add Route
    Add Route

--- src/App.js
+++ src/App.js
@@ -7,21 +7,28 @@ import React from 'react'
 //import RefsFunc from './refs-class'
 //import RefsArray from './refs-array'
 //import MessageBox from './state-class'
-import Context from './context-content'
-import { userContext } from './context'
-import Header2 from './context-header2'
-import Context2 from './context-content2'
+//import Context from './context-content'
+//import { userContext } from './context'
+//import Header2 from './context-header2'
+//import Context2 from './context-content2'
+import Router2 from './router2'
+import Router3 from './router3'
 
-
-export default function App(){
-  let [user,setUser] = React.useState('')
-  return(
-    <userContext.Provider value={[user,setUser]}>
-      <Context2/>
-      <Header2/>
-      </userContext.Provider>
+export default function App() {
+  return (
+    <Router3/>
   )
+}
+
+//export default function App(){
+  //let [user,setUser] = React.useState('')
+  //return(
+   // <userContext.Provider value={[user,setUser]}>
+   //   <Context2/>
+    //  <Header2/>
+    //  </userContext.Provider>
+ // )
   //return <RefsFunc/>
   //return <RefsArray/>
   //return <MessageBox/> 
-} 
+//} 
