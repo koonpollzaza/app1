05097b0fd6a4588b75ed15d0407a039308e37b45
koonpollzaza <152575123+koonpollzaza@users.noreply.github.com>
2025-02-26 16:01:41 +0700

    add form-select-file
    add form-select-file

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ import React from 'react'
 //import Context2 from './context-content2'
 //import Router2 from './router2'
 //import Router3 from './router3'
-import FormText from './form-text'
+//import FormText from './form-text'
+import FormSelectFile from './form-select-file'
 
 export default function App() {
   return (
     //<Router3/>
-    <FormText/>
+    <FormSelectFile/>
+    //<FormText/>
   )
 }
 
